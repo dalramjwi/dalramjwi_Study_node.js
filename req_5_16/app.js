83210f1c7b2b413f45a5643928e03270c5e110e9
dalramjwi <dalramjwi@gmail.com>
2024-05-16 13:59:56 +0900

    use get - make file complete version

--- req_5_16/app.js
+++ req_5_16/app.js
@@ -10,15 +10,32 @@ const server = http.createServer((req, res) => {
     console.log(qstring);
     if (req.url === "/") {
       res.writeHead(200, { "Content-Type": "text/html" });
-      const html = fs.readFileSync("./public/index.html");
+      const html = fs.readFileSync("./public/indexOne.html");
       res.end(html);
     }
     if (req.url.startsWith("/test")) {
       const inputData = qurl.split("?")[1];
       const data = qs.decode(inputData);
       console.log(data);
+      let filenameArr = [];
+      // console.log(filenameArr);
+      let filetextArr = [];
+      // console.log(filetextArr);
+      filenameArr.push(data.name);
+      filetextArr.push(data.text);
+      // for (let i = 0; i < filenameArr.length; i++) {
+      if (filenameArr.length > 0) {
+        fs.writeFile(
+          `./public/index${filenameArr[0]}.html`,
+          `${filetextArr[0]}`,
+          (err) => {
+            console.log(err);
+          }
+        );
+      }
     }
   }
+  // }
 
   if (req.method === "POST") {
   }
