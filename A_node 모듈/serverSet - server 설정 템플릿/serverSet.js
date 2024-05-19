7a643b75ad4ea5f7a82aee8d2a6b0e99dad75519
dalramjwi <168498602+dalramjwi@users.noreply.github.com>
2024-05-20 00:03:40 +0900

    server module make

--- A_node 모듈/serverSet - server 설정 템플릿/serverSet.js
+++ A_node 모듈/serverSet - server 설정 템플릿/serverSet.js
@@ -1,20 +1,64 @@
-const http = require("http");
-const fs = require("fs");
-const qs = require("node:querystring");
-const memberName = require("./public/data.js");
-// const namearr = [];
-const server = http.createServer((req, res) => {
-  let url = req.url;
-  console.log(url);
-  if (req.method === "GET") {
-    if (req.url === "/") {
-      res.writeHead(200, { "Content-Type": "text/html" });
-      const html = fs.readFileSync("./public/index.html");
-      res.end(html);
-    }
+const serverSet = function serverSet(port) {
+  const http = require("http");
+  const fs = require("fs");
+  const path = require("path");
+  //*문서 형식에 따른 표기
+  const mimeType = {
+    ".html": "text/html; charset=utf-8",
+    ".css": "text/css",
+    ".js": "application/javascript",
+    ".json": "application/json",
+    ".ico": "image/x-icon",
+    ".jpg": "image/jpeg",
+    ".jpeg": "image/jpeg",
+    ".png": "image/png",
+    ".gif": "image/gif",
+  };
+
+  //*url에 따른 파일 경로 결정 함수 객체
+  const fileUtils = {
+    //*매개변수 url에 따른 파일 경로 할당
+    getFilePath: function (url) {
+      let filePath;
+      if (url === "/") {
+        filePath = "./public/index.html";
+      } else {
+        filePath = `./public${url}`;
+      }
+      return filePath;
+    },
+    //*파일 경로에 따른 파일 확장자 가져오기
+    getFileExtension: function (filePath) {
+      //*파일 확장자를 가져오는 명령어
+      let ext = path.extname(filePath);
+      //*파일 확장자 소문자로 변환
+      return ext.toLowerCase();
+    },
+    //*파일 확장자에 따른 표기 반환
+    getContentType: function (ext) {
+      //*mimeType에 ext로 가져온 확장자가 있다면 표기 반환
+      if (mimeType.hasOwnProperty(ext)) {
+        return mimeType[ext];
+      } else {
+        return "text/plain";
+      }
+    },
+  };
+
+  //*get 요청일때 처리 함수
+  function getMethod(req, res, filePath, contentType) {
+    fs.readFile(filePath, (err, data) => {
+      if (err) {
+        console.log("오류 발생", err);
+      } else {
+        res.writeHead(200, { "Content-Type": contentType });
+        res.end(data);
+      }
+    });
   }
 
-  if (req.method === "POST") {
+  //*post 요청일때 처리 함수
+  function postMethod(req, res) {
     let body = "";
     req.on("data", (data) => {
       body += data;
@@ -22,66 +66,49 @@ const server = http.createServer((req, res) => {
     req.on("end", () => {
       let parse = qs.parse(body);
       console.log(parse);
-
-      for (let i = 0; i < memberName.length; i++) {
-        if (parse.name === memberName[i]) {
-          res.statusCode = 201;
-          return (res.statusCode = 201);
-        } else if (parse.name !== memberName[i]) {
-          res.statusCode = 404;
-        }
-        console.log(res.statusCode);
-      }
     });
   }
 
-  if (url === "/index.js") {
-    FileRead("./public/index.js", (data) => {
-      res.writeHead(200, { "Content-Type": "application/javascript" });
-      res.write(data);
-      res.end();
-    });
-  }
-  if (url === "/formSet.js") {
-    FileRead("./public/formSet.js", (data) => {
-      res.writeHead(200, { "Content-Type": "application/javascript" });
-      res.write(data);
-      res.end();
-    });
-  }
-  if (url === "/data.js") {
-    FileRead("./public/data.js", (data) => {
-      res.writeHead(200, { "Content-Type": "application/javascript" });
-      res.write(data);
-      res.end();
-    });
-  }
-  if (url === "/inputSet.js") {
-    FileRead("./public/inputSet.js", (data) => {
-      res.writeHead(200, { "Content-Type": "application/javascript" });
-      res.write(data);
-      res.end();
-    });
-  }
-  if (url === "/buttonSet.js") {
-    FileRead("./public/buttonSet.js", (data) => {
-      res.writeHead(200, { "Content-Type": "application/javascript" });
-      res.write(data);
-      res.end();
-    });
-  }
-});
+  //*서버 생성
+  const server = http.createServer((req, res) => {
+    let url = req.url;
+    if (req.url === "/favicon.ico") return;
+    //*filePath라는 변수를 getFilePath에 req.url을 매개변수로 삽입한 값으로 할당
+    let filePath = fileUtils.getFilePath(url);
+    //*ext 변수는 getFileExtenstion에 filePath를 삽입한 값으로 할당
+    let ext = fileUtils.getFileExtension(filePath);
+    //*contentType 변수는 getContentType에 ext를 삽입한 값으로 할당
+    let contentType = fileUtils.getContentType(ext);
 
-server.listen(8080, () => {
-  console.log("Server is running on port 8080");
-});
+    if (req.method === "GET") {
+      getMethod(req, res, filePath, contentType);
+    } else if (req.method === "POST") {
+      postMethod(req, res);
+    }
 
-function FileRead(path, callback) {
-  fs.readFile(path, (err, data) => {
+    // fs.readFile(filePath, (err, data) => {
+    //   if (err) {
+    //     console.log("오류 발생", err);
+    //   } else {
+    //     res.writeHead(200, { "Content-Type": contentType });
+    //     res.end(data);
+    //   }
+    // });
+  });
+
+  server.listen(port, (err) => {
     if (err) {
-      console.log(err);
+      console.log("오류:", err);
     } else {
-      callback(data);
+      console.log(`Server is running on port ${port}`);
     }
   });
-}
+};
+
+//*매개변수 port 작성법
+//*serverSet(3000);
+
+//? export한 데이터 받아오는 명령어
+//?const serverSet = require("./serverSet");
+
+module.exports = serverSet;
