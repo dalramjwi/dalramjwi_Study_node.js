59fcdbd5baedd49e33cd3e326f22c648c7cf3850
dalramjwi <dalramjwi@gmail.com>
2024-05-23 15:55:39 +0900

    pop use

--- Readline_5_23/solve.js
+++ Readline_5_23/solve.js
@@ -16,8 +16,12 @@ const ques = () => {
     if (answerData.includes(answer)) {
       // checkData = 1;
       // if (checkData === 1) {
-      console.log("!!! 값이 중복됩니다. 종료됩니다. !!!");
-      console.log("당신의 점수 : " + score);
+      console.log("!!! 값이 중복됩니다. !!!");
+      // console.log("당신의 점수 : " + score);
+      answerData.pop();
+      // console.log(answerData);
+      score--;
+      ques();
       // }
       // readLine.close();
     }
@@ -25,27 +29,29 @@ const ques = () => {
       console.log(answer + " : 존재합니다.");
       answerData.push(answer);
       score++;
-      console.log(answerData);
-      console.log("당신의 점수 : " + score);
+      // console.log(answerData);
+      // console.log("당신의 점수 : " + score);
       ques();
     } else if ("종료" === answer) {
       console.log("당신의 점수 : " + score);
       readLine.close();
     } else if (pokemonData.includes(answer) === false) {
-      console.log(answer + " : 존재하지 않습니다.");
+      console.log(
+        answer + " : 존재하지 않습니다. 한번의 기회가 더 주어집니다."
+      );
       readLine.question("포켓몬스터 이름 작성해주세요 :", function (answer) {
         if (pokemonData.includes(answer)) {
           console.log(answer + " : 존재합니다.");
           answerData.push(answer);
           score++;
-          console.log(answerData);
-          console.log("당신의 점수 : " + score);
+          // console.log(answerData);
+          // console.log("당신의 점수 : " + score);
           ques();
         } else if ("종료" === answer) {
           console.log(score);
         } else if (pokemonData.includes(answer) === false) {
           console.log(answer + " : 존재하지 않습니다. 실패했습니다.");
-          console.log("당신의 점수 : " + score);
+          // console.log("당신의 점수 : " + score);
           // readLine.close();
         }
       });
