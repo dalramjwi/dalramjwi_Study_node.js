633748c986c1f969fca5a6b1c318476784a275c1
dalramjwi <dalramjwi@gmail.com>
2024-05-24 14:29:56 +0900

    update

---
+++ subject_5/public/index.js
@@ -0,0 +1,5 @@
+import { formSet } from "./formSet.js";
+const formData = ["actionPath", "method", "path"];
+const inputData = ["type", "name", "placeholder"];
+const buttonData = ["submit", "누르시오"];
+formSet(formData, inputData, buttonData);
