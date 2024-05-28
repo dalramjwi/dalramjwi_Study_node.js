6b11eae6210dbcd0b0c3fcb5de93a39b00207da0
dalramjwi <dalramjwi@gmail.com>
2024-05-28 14:30:14 +0900

    template 수정

--- subject_5/my_module/literalTemplate.js
+++ subject_5/my_module/literalTemplate.js
@@ -1,10 +1,10 @@
 const template = {
   htmlTempalte: function (title, content, tag) {
     return `<!DOCTYPE html>
-  <html>
+  <html lang="en">
   <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>${title}</title>
-    <meta charset="utf-8">
   </head>
   <body>
   <h1>${title}</h1>
@@ -17,7 +17,6 @@ const template = {
     return `<!DOCTYPE html>
 <html lang="en">
   <head>
-    <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Document</title>
     <link rel="stylesheet" href="index.css" />
