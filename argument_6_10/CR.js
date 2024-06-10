f79f87afa20e4e088cd15854eb706570941550ab
dalramjwi <dalramjwi@gmail.com>
2024-06-10 10:57:00 +0900

    complete

--- argument_6_10/CR.js
+++ argument_6_10/CR.js
@@ -32,3 +32,52 @@ class Person {
     return this._job;
   }
 }
+//생성된 person 생성자 함수를 사용하고, 후에 값을 할당하고 있다.
+const person = new Person();
+person.name = "김달";
+person.age = "11";
+person.job = "자택경비";
+//console.log로 작성된 값 확인
+console.log("Name : ", person.name);
+console.log("Age : ", person.age);
+console.log("Job : ", person.job);
+
+/**
+ * 화살표 함수로 콜백 함수이다.
+ * @param {function} callback person이라는 빈 객체를 만들고, callback 함수를 실행한다.
+ */
+const createPerson = (callback) => {
+  let person = {};
+  callback(person, setName);
+};
+/**
+ * 콜백 함수이다.
+ * @param {object} person createPerson에서 정의한 빈 객체이다
+ * @param {function} callback callback 함수이다.
+ */
+const setName = (person, callback) => {
+  //빈 객체에 key 값과 value 값을 후에 할당하고 있다.
+  person.name = "배고파";
+  callback(person, setAge);
+};
+/**
+ * 콜백 함수이다.
+ * @param {object} person person이라는 객체 안에 name 값이 추가된 객체이다.
+ * @param {function} callback  callback 함수.
+ */
+const setAge = (person, callback) => {
+  person.age = "11";
+  callback(person, setJob);
+};
+/**
+ * person 객체에 값을 할당하고 console로 확인하는 콜백 함수이다.
+ * @param {object} person name과 age 값이 추가되어 있는 person 객체
+ */
+const setJob = (person) => {
+  person.job = "밥";
+  console.log(person);
+};
+//createPerson 함수를 부르고, 매개변수로 콜백함수를 넣었다.
+//매개변수에 넣은 콜백함수는 setName 콜백함수이다.
+//인자를 전달하기 위해 다음과 같은 형태로 작성했다.
+createPerson((person, callback) => callback(person, setName));
